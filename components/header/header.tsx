f1a53c9570a14d7ffb3d4d39d09d83adffbfc3a4
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-21 11:58:15 +0330

    Add devices links to main header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -9,10 +9,33 @@ function Header() {
       </h1>
       <nav aria-label="main-navigation">
         <ul>
-            <li><Link href="/devices">Devices</Link></li>
-            <li><Link href="/compare">Compare</Link></li>
-            <li><Link href="/news">News</Link></li>
-            <li><Link href="/about">About</Link></li>
+          {/* Devices links*/}
+          <li>
+            <Link href="/devices">Devices</Link>
+            <ul>
+              <li>
+                <Link href="/devices/smartphone">Smartphone</Link>
+              </li>
+              <li>
+                <Link href="/devices/laptop">Laptop</Link>
+              </li>
+              <li>
+                <Link href="/devices/tablet">Tablet</Link>
+              </li>
+            </ul>
+          </li>
+          {/* Compare link */}
+          <li>
+            <Link href="/compare">Compare</Link>
+          </li>
+          {/* News link */}
+          <li>
+            <Link href="/news">News</Link>
+          </li>
+          {/* About Link */}
+          <li>
+            <Link href="/about">About</Link>
+          </li>
         </ul>
       </nav>
     </header>
