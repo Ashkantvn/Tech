6f7a146fbb85770634193e76aa897d5c55b16067
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-23 17:20:09 +0430

    Create global metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import Header from "@/components/header/header";
+import { Metadata } from "next";
 
-export const metadata = {};
+export const metadata  = {
+  title: "TECH - news, and information about digital devices",
+  viewport: "width=device-width, initial-scale=1.0",
+  description:
+    "The website, helps you to check news, compare devices, and get some information about digital devices.",
+  keywords:
+    "technology, technology news, digital devices, digital products, electronic device, smartphone, laptop, tablet",
+};
 
 export default function RootLayout({
   children,
