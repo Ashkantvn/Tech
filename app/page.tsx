1b4a69a20276c1bf38e8f250b6efb5eb8d72f2cd
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-29 16:06:25 +0430

    Add hero section style for small screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { Inter } from "next/font/google";
+import { GiProcessor } from "react-icons/gi";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
   return (
-    <main>
-      <section>
-        <h1>TECH</h1>
-        <h2>an online magazine for dgital devices.</h2>
-        <p>
+    <main className="mt-6">
+      <section className="flex flex-col items-center w-3/4 mx-auto text-center">
+        <div>
+          <GiProcessor size="12em"/>
+        </div>
+        <h1 className=" font-extrabold">TECH</h1>
+        <h2 className="my-3 font-semibold opacity-60">an online magazine for dgital devices.</h2>
+        <p className="text-xs opacity-30">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
           reprehenderit suscipit, doloremque quos ut, explicabo voluptate
           commodi, alias impedit tempore labore ipsam quis atque voluptas quas
