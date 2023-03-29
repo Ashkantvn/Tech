6ab29f136b79f9664b53f12e9eb210a1d79e2303
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-29 17:49:58 +0430

    Add hero section for large screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,29 @@ import { GiProcessor } from "react-icons/gi";
 const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
+  const heroSectionClassNames: () => string = () => {
+    const heroSectionMainClasses: string ="flex flex-col items-center w-3/4 mx-auto text-center";
+    const heroSectionLgClasses: string = "lg:w-2/3 lg:flex-row-reverse lg:justify-around lg:mt-32 lg:w-full lg:text-left";
+    return `${heroSectionMainClasses} ${heroSectionLgClasses}`;
+  };
   return (
     <main className="mt-6">
-      <section className="flex flex-col items-center w-3/4 mx-auto text-center">
+      <section className={heroSectionClassNames()}>
         <div>
           <GiProcessor size="12em"/>
         </div>
-        <h1 className=" font-extrabold">TECH</h1>
-        <h2 className="my-3 font-semibold opacity-60">an online magazine for dgital devices.</h2>
-        <p className="text-xs opacity-30">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-          reprehenderit suscipit, doloremque quos ut, explicabo voluptate
-          commodi, alias impedit tempore labore ipsam quis atque voluptas quas
-          voluptatem ipsa eum? Reprehenderit!
-        </p>
+        <div className="lg:w-96">
+          <h1 className="font-extrabold lg:hidden">TECH</h1>
+          <h2 className="my-3 font-semibold opacity-60 lg:opacity-80">
+            an online magazine for dgital devices.
+          </h2>
+          <p className="text-xs opacity-30">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+            reprehenderit suscipit, doloremque quos ut, explicabo voluptate
+            commodi, alias impedit tempore labore ipsam quis atque voluptas quas
+            voluptatem ipsa eum? Reprehenderit!
+          </p>
+        </div>
       </section>
     </main>
   );
