aa19d78ff610b5bcf48c124d3e2f85c4eaef3fe1
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-29 14:52:04 +0430

    Create content of hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,20 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
-  return <>HomePage</>;
+  return (
+    <main>
+      <section>
+        <h1>TECH</h1>
+        <h2>an online magazine for dgital devices.</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+          reprehenderit suscipit, doloremque quos ut, explicabo voluptate
+          commodi, alias impedit tempore labore ipsam quis atque voluptas quas
+          voluptatem ipsa eum? Reprehenderit!
+        </p>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
