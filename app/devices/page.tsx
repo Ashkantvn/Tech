4a1c9a40cbe64004a39d544eac3453a4d80f1726
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-05 13:37:02 +0430

    Create content of devices types

--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
 import React from "react";
+import { BsLaptop, BsPhone, BsSearch, BsTablet } from "react-icons/bs";
 
 function Devicespage() {
-  return <div>DevicesPage</div>;
+  return (
+    <main>
+      <h1>Devices page</h1>
+      <label>
+        <h4>Search your favourite device:</h4>
+        <input type="search" />
+        <button>
+          <BsSearch />
+        </button>
+      </label>
+
+      <div>
+        <h3>Devices types:</h3>
+        <ul>
+          <li>
+            <Link href="/devices/smartphone">
+              <BsPhone />
+              <h2>Smartphone</h2>
+            </Link>
+          </li>
+          <li>
+            <Link href="/devices/tablet">
+              <BsTablet />
+              <h2>Tablet</h2>
+            </Link>
+          </li>
+          <li>
+            <Link href="/devices/laptops">
+              <BsLaptop />
+              <h2>Laptop</h2>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
 }
 
 export default Devicespage;
