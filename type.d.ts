041693b5a2c6d6178bac2cd6f6c72b87ea694038
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-05 14:00:44 +0430

    Create type for devices

---
+++ type.d.ts
@@ -0,0 +1,12 @@
+type device = {
+  id: number,
+  device_name: string,
+  announced: string,
+  os: string,
+  display: string,
+  memory: string,
+  camera: string,
+  battery: string,
+  price: string,
+  type: string,
+};
