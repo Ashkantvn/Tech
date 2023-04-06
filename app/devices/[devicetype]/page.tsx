8219d569a7d367bab3ac1f9ab0d69e45437950d8
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-07 00:31:38 +0430

    Add fetch function and use device in devicetype page

--- app/devices/[devicetype]/page.tsx
+++ app/devices/[devicetype]/page.tsx
@@ -1,15 +1,34 @@
-"use client"
+"use client";
+import Card from "@/components/card/card";
+import getAllDevices from "@/lib/getAllDevices";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function DeviceTypePage() {
-  const pathName :string =  usePathname().slice(9);
-  return (<main>
+  const pathName: string = usePathname().slice(9);
+  const [devices, setdevice] = useState<device[]>();
+  const mapDevices: React.ReactNode = devices?.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  //fetch functions
+  const fetchSmartphone = async () => {
+    const devicesData: device[] = await getAllDevices();
+    const filterDevices: device[] = devicesData.filter((item) =>
+      item.type.toLowerCase().includes(pathName)
+    );
+    setdevice(filterDevices);
+  };
+  useEffect(() => {
+    fetchSmartphone();
+  }, []);
+
+  return (
+    <div>
       <h1>{pathName}</h1>
-    
-    </main>
-  )
-  ;
+      <ul>{mapDevices}</ul>
+    </div>
+  );
 }
 
 export default DeviceTypePage;
