601b357764a64a3c1a9be43c0043b9de98cc5825
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-07 14:19:45 +0430

    Add style of devicetype page

--- app/devices/[devicetype]/page.tsx
+++ app/devices/[devicetype]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Card from "@/components/card/card";
 import getAllDevices from "@/lib/getAllDevices";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { BiLeftArrowCircle } from "react-icons/bi";
 
 function DeviceTypePage() {
   const pathName: string = usePathname().slice(9);
@@ -24,9 +26,10 @@ function DeviceTypePage() {
   }, []);
 
   return (
-    <div>
-      <h1>{pathName}</h1>
-      <ul>{mapDevices}</ul>
+    <div className="mb-12">
+      <Link className="text-2xl" href="/devices"><BiLeftArrowCircle/></Link>
+      <h1 className="border-b p-3 text-center my-12 font-bold text-lg">{pathName}</h1>
+      <ul className="grid grid-cols-1 gap-y-8 md:grid-cols-2 md:w-4/5 md:gap-x-3 mx-auto lg:grid-cols-3">{mapDevices}</ul>
     </div>
   );
 }
