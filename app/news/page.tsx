82e15bb7ccfe14f28a6777bd97af498b6428b080
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-10 10:39:30 +0430

    Use news_data in news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,26 @@
+import getAllNews from "@/lib/getAllNews";
+import Link from "next/link";
 import React from "react";
 
-function News() {
-  return <div>NewsPage</div>;
+async function News() {
+  const newsData: Promise<news[]> = getAllNews();
+  const news: news[] = await newsData;
+  const mappedNews: React.ReactNode = news.map((item: news) => {
+    return (
+      <li key={item.id}>
+        <h3>{item.title}</h3>
+        <p>{item.content}</p>
+        <Link href={`news/${item.id}`}>More</Link>
+      </li>
+    );
+  });
+
+  return (
+    <main>
+      <h1>News page</h1>
+      <ul>{mappedNews}</ul>
+    </main>
+  );
 }
 
 export default News;
