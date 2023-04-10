77c01489cd94b9c8c447e0863c48ee4154faa85f
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-10 10:00:22 +0430

    Correct api

--- lib/getAllNews.ts
+++ lib/getAllNews.ts
@@ -1,5 +1,5 @@
 export default async function getAllNews(){
-    const response= await fetch("http://localhost:3000/api/devices");
+    const response= await fetch("http://localhost:3000/api/news");
     if(!response.ok) throw Error("failed to fetch data");
     return response.json();
 }
