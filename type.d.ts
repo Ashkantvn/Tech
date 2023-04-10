fc5819b708ec924eef9d9749001fe6740371ca43
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-10 09:57:44 +0430

    Add news type

--- type.d.ts
+++ type.d.ts
@@ -1,12 +1,20 @@
 type device = {
-  id: number,
-  device_name: string,
-  announced: string,
-  os: string,
-  display: string,
-  memory: string,
-  camera: string,
-  battery: string,
-  price: string,
-  type: string,
+  id: number;
+  device_name: string;
+  announced: string;
+  os: string;
+  display: string;
+  memory: string;
+  camera: string;
+  battery: string;
+  price: string;
+  type: string;
+};
+type news = {
+  id: number;
+  title: string;
+  content: string;
+  keywords: string;
+  author: string;
+  date: string;
 };
