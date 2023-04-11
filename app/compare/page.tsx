6f11d014c11440d40766710e8f65fd877c756247
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-11 23:01:10 +0430

    Add main content of compare page

--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 function Compare() {
-  return <div>ComparePage</div>;
+  return (
+    <main>
+      <label>
+        Select type of device :
+        <select name="device-type" id="">
+          <option value="Smartphone">Smartphone</option>
+          <option value="Laptop">Laptop</option>
+          <option value="Tablet">Tablet</option>
+        </select>
+      </label>
+      <article >
+        <section>
+          <h3>Device - 1</h3>
+          <ul>
+            <li>
+              <span>announced :</span>
+            </li>
+            <li>
+              <span>display :</span>
+            </li>
+            <li>
+              <span>camera :</span>
+            </li>
+            <li>
+              <span>memory :</span>
+            </li>
+            <li>
+              <span>OS :</span>
+            </li>
+            <li>
+              <span>battery :</span>
+            </li>
+            <li>
+              <span>price :</span>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>Device - 2</h3>
+          <ul>
+            <li>
+              <span>announced :</span>
+            </li>
+            <li>
+              <span>display :</span>
+            </li>
+            <li>
+              <span>camera :</span>
+            </li>
+            <li>
+              <span>memory :</span>
+            </li>
+            <li>
+              <span>OS :</span>
+            </li>
+            <li>
+              <span>battery :</span>
+            </li>
+            <li>
+              <span>price :</span>
+            </li>
+          </ul>
+        </section>
+      </article>
+    </main>
+  );
 }
 
 export default Compare;
