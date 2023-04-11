b11ff225daa74f167694825f57b428c14fe22dda
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-11 22:45:39 +0430

    Update style of news-id page

--- app/news/[news-id]/page.tsx
+++ app/news/[news-id]/page.tsx
@@ -19,10 +19,10 @@ function SpecificNews() {
     fetchNews();
   }, []);
   return (
-    <div className="m-9">
+    <div className="m-9 md:w-3/4 md:mx-auto lg:mt-16">
       <h1 className="font-bold text-sm">{news?.title}</h1>
       <p className="my-9">{news?.content}</p>
-      <h6 className="m-9 opacity-90">important words: <br /> {news?.keywords}</h6>
+      <h6 className="my-9 opacity-90"><span className="font-bold text-sm">important words:</span> <br /> {news?.keywords}</h6>
       <address className="opacity-30">
         <span>Written by : {news?.author}</span><br />
         <time dateTime={news?.date}>{news?.date}</time>
