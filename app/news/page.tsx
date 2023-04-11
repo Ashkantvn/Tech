5bc04d5b67a9f2c75a60162288f5120816f1c371
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-11 22:13:33 +0430

    Add style of every news

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,7 @@
 import getAllNews from "@/lib/getAllNews";
 import Link from "next/link";
 import React from "react";
+import style from "@/style/news.module.css";
 
 async function News() {
   const newsData: Promise<news[]> = getAllNews();
@@ -8,9 +9,9 @@ async function News() {
   const mappedNews: React.ReactNode = news.map((item: news) => {
     return (
       <li key={item.id}>
-        <h3>{item.title}</h3>
-        <p >{item.content}</p>
-        <Link href={`news/${item.id}`}>More</Link>
+        <h3 className="font-bold text-lg">{item.title}</h3>
+        <p className={style.ellipsis+" opacity-50"}>{item.content}</p>
+        <Link className="font-bold text-sm" href={`news/${item.id}`}>More</Link>
       </li>
     );
   });
