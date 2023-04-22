bf4ae5865b39e78ca8c894dcfb07242994ac5e56
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-22 19:31:03 +0430

    Use params instead of hooks

--- app/devices/[devicetype]/[device]/page.tsx
+++ app/devices/[devicetype]/[device]/page.tsx
@@ -1,57 +1,52 @@
-"use client";
 import getAllDevices from "@/lib/getAllDevices";
-import { usePathname } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import React from "react";
 import { BiLeftArrowCircle } from "react-icons/bi";
 
-function DevicePage() {
-  const deviceId: number = Number(usePathname().replace(/[^0-9]/g, ""));
-  const [device, setDevice] = useState<device>();
+async function DevicePage({ params }: { params: { devicetype: string ,device:string } }) {  
+  const deviceId: number = Number(params.device);
+  const devicesData: device[] = await getAllDevices();
 
-  //fetch data
-  useEffect(() => {
-    const fetchDevice = async () => {
-      const devicesData: device[] = await getAllDevices();
-      const filterDevices: device = devicesData.filter(
-        (item) => item.id === deviceId
-      )[0];
-      setDevice(filterDevices);
-    };
-    fetchDevice();
-  }, []);
+
+  ////filter devices
+  const filterDevices: device = devicesData.filter(
+    (item) => item.id === deviceId
+  )[0];
+  
 
   return (
     <div className="mt-16 w-4/5 mx-6 md:mx-auto">
-      <button
+      <Link
         className="text-xl"
-        onClick={() => {
-          history.back();
-        }}
+        href="/devices"
       >
         <BiLeftArrowCircle />
-      </button>
-      <h1 className="text-xl font-bold">{device?.device_name}</h1>
+      </Link>
+
+
+      {/* device informations */}
+      <h1 className="text-xl font-bold">{filterDevices?.device_name}</h1>
       <ul className="mx-12 flex flex-col gap-9 mt-9 mb-16 [&>li>span]:block">
         <li>
-          <span>announced :</span> {device?.announced}
+          <span>announced :</span> {filterDevices?.announced}
         </li>
         <li>
-          <span>display :</span> {device?.display}
+          <span>display :</span> {filterDevices?.display}
         </li>
         <li>
-          <span>camera :</span> {device?.camera}
+          <span>camera :</span> {filterDevices?.camera}
         </li>
         <li>
-          <span>memory :</span> {device?.memory}
+          <span>memory :</span> {filterDevices?.memory}
         </li>
         <li>
-          <span>OS :</span> {device?.os}
+          <span>OS :</span> {filterDevices?.os}
         </li>
         <li>
-          <span>battery :</span> {device?.battery}
+          <span>battery :</span> {filterDevices?.battery}
         </li>
         <li>
-          <span>price :</span> {device?.price}
+          <span>price :</span> {filterDevices?.price}
         </li>
       </ul>
     </div>
