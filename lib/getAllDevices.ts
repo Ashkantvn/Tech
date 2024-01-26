3b6618135231bf2566b14865011bace224c13288
Ashkan <ashkantavangar7@gmail.com>
2024-01-26 22:07:43 +0330

    Update url

--- lib/getAllDevices.ts
+++ lib/getAllDevices.ts
@@ -1,5 +1,5 @@
 export default async function getAllDevices(){
-    const response = await fetch("http://localhost:3000/api/devices");
+    const response = await fetch("https://radiant-froyo-fde92e.netlify.app/api/devices");
     if(!response.ok) throw new Error("failed to fetch data");
     return response.json();
 }
--- lib/getAllNews.ts
+++ lib/getAllNews.ts
@@ -1,5 +1,5 @@
 export default async function getAllNews(){
-    const response= await fetch("http://localhost:3000/api/news");
+    const response= await fetch("https://radiant-froyo-fde92e.netlify.app/api/news");
     if(!response.ok) throw Error("failed to fetch data");
     return response.json();
 }
