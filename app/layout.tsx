78aab836d1a6c0493031653cb2e45eba7503b97b
Ashkan <ashkantavangar7@gmail.com>
2024-01-26 13:46:23 +0330

    Update config, and change layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "../style/global.css";
 
 export const metadata  = {
   title: "TECH - news, and information about digital devices",
-  viewport: "width=device-width, initial-scale=1.0",
   description:
     "The website, helps you to check news, compare devices, and get some information about digital devices.",
   keywords:
@@ -17,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
       <body>
         <Header />
         {children}
